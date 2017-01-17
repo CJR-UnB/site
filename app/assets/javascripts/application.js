762de535838ec6a071538db5d43a2a421cf91759
Rafael Fernandes <spnrafael@gmail.com>
2017-01-17 18:29:27 -0200

    Menu overlay e animatescroll funcional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,31 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require parallax.min
-//= require modernizr.custom
-//= require snap.svg-min
-//= require classie
-//= require overlay 
+//= require animatescroll
 //= require turbolinks
 //= require_tree .
+
+function sobreLinkClick() {
+  $('.sobre-box').animatescroll();
+  $("#op").prop("checked", false);
+}
+
+function servicosLinkClick() {
+  $('.servicos-box').animatescroll();
+  $("#op").prop("checked", false);
+}
+
+function portfolioLinkClick() {
+  $('.portfolio-box').animatescroll();
+  $("#op").prop("checked", false);
+}
+
+function membrosLinkClick() {
+  $('.membros-box').animatescroll();
+  $("#op").prop("checked", false);
+}
+
+function contatoLinkClick() {
+  $('.contato-box').animatescroll();
+  $("#op").prop("checked", false);
+}
